5e3f0d187aef989f52df2354534aaf9642e5fc81
Jacob Williams <jacobaw@gmail.com>
2018-01-25 18:53:14 -0600

    pointless action creator tests

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
-export function setWordHinted(word, hinted) {
-  return { type: 'SET_WORD_HINTED', word, hinted };
-}
-
 export function attemptSolution(start, end) {
   return { type: 'ATTEMPT_SOLUTION', start, end };
+}
+
+export function setWordHinted(word, hinted) {
+  return { type: 'SET_WORD_HINTED', word, hinted };
 }
---
+++ src/actions.test.js
@@ -0,0 +1,25 @@
+import { attemptSolution, setWordHinted } from './actions';
+
+describe('attemptSolution action creator', () => {
+  it('creates the action', () => {
+    const expected = {
+      type: 'ATTEMPT_SOLUTION',
+      start: {x: 1, y: 2},
+      end: {x: 3, y: 4}
+    };
+    const result = attemptSolution({x: 1, y: 2}, {x: 3, y: 4});
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('setWordHinted action creator', () => {
+  it('creates the action', () => {
+    const expected = {
+      type: 'SET_WORD_HINTED',
+      word: 'HUNGRY',
+      hinted: true
+    };
+    const result = setWordHinted('HUNGRY', true);
+    expect(result).toEqual(expected);
+  });
+});
