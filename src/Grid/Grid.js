5eaab6faa1fcb4f9178f2afd599faaa82ad358ad
Jacob Williams <jacobaw@gmail.com>
2018-01-25 18:07:50 -0600

    get basic word solving working, needs tests and cleanup

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { attemptSolution } from '../actions';
 import './Grid.css';
 
 const CELL_WIDTH = 30;
@@ -7,36 +8,111 @@ const CELL_HEIGHT = 30;
 const CELL_FONT_SIZE = 24;
 const CELL_MIDDLE_Y = 22; // unsure if this number just happens to work or has significance
 
-export function Grid({ grid, hintedWordPositions }) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg"
-         width={30*grid[0].length} height={30*grid.length}
-         className="Grid">
-      {grid.map((row, y) =>
-        row.map((letter, x) => (
-          <text key={[x, y]}
-                x={x * CELL_WIDTH + (CELL_WIDTH / 2)}
-                y={(y + 1) * CELL_HEIGHT}
-                font-size={CELL_FONT_SIZE}
-                text-anchor="middle">
-            {letter}
-          </text>)))}
-      {hintedWordPositions.map(({word, start, end}) => (
-        <line className="hint"
-              key={word}
-              x1={start.x * CELL_WIDTH + (CELL_WIDTH / 2)}
-              y1={start.y * CELL_HEIGHT + CELL_MIDDLE_Y}
-              x2={end.x * CELL_WIDTH + (CELL_WIDTH / 2)}
-              y2={end.y * CELL_HEIGHT + CELL_MIDDLE_Y} />))}
-    </svg>
-  );
+export class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      proposedSolution: null
+    };
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+  }
+
+  handleMouseDown(event) {
+    const coords = {
+      x: event.clientX - this.svg.getBoundingClientRect().left,
+      y: event.clientY - this.svg.getBoundingClientRect().top
+    };
+    this.setState({
+      proposedSolution: {
+        start: coords,
+        end: coords
+      }});
+    event.preventDefault();
+  }
+
+  handleMouseMove(event) {
+    const coords = {
+      x: event.clientX - this.svg.getBoundingClientRect().left,
+      y: event.clientY - this.svg.getBoundingClientRect().top
+    }
+    this.setState(prevState => {
+      if (prevState.proposedSolution) {
+        return {
+          proposedSolution: {
+            start: prevState.proposedSolution.start,
+            end: coords
+          }
+        }
+      }
+    });
+    event.preventDefault();
+  }
+
+  handleMouseUp(event) {
+    if (this.state.proposedSolution) {
+      this.props.attemptSolution(
+        {
+          x: Math.floor(this.state.proposedSolution.start.x / 30),
+          y: Math.floor(this.state.proposedSolution.start.y / 30)
+        },
+        {
+          x: Math.floor(this.state.proposedSolution.end.x / 30),
+          y: Math.floor(this.state.proposedSolution.end.y / 30)
+        }
+      )
+    }
+    this.setState({ proposedSolution: null });
+    event.preventDefault();
+  }
+
+  render() {
+    const { grid, words } = this.props;
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg"
+           width={30*grid[0].length} height={30*grid.length}
+           className="Grid"
+           onMouseDown={this.handleMouseDown}
+           onMouseMove={this.handleMouseMove}
+           onMouseUp={this.handleMouseUp}
+           ref={svg => this.svg = svg} >
+        {grid.map((row, y) =>
+          row.map((letter, x) => (
+            <text key={[x, y]}
+                  x={x * CELL_WIDTH + (CELL_WIDTH / 2)}
+                  y={(y + 1) * CELL_HEIGHT}
+                  fontSize={CELL_FONT_SIZE}
+                  textAnchor="middle">
+              {letter}
+            </text>)))}
+        {words.filter(word => word.hinted || word.solved)
+              .map(({word, start, end, hinted, solved}) => (
+          <line className={hinted ? 'hint' : 'solution'}
+                key={word}
+                x1={start.x * CELL_WIDTH + (CELL_WIDTH / 2)}
+                y1={start.y * CELL_HEIGHT + CELL_MIDDLE_Y}
+                x2={end.x * CELL_WIDTH + (CELL_WIDTH / 2)}
+                y2={end.y * CELL_HEIGHT + CELL_MIDDLE_Y} />))}
+
+        {this.state.proposedSolution &&
+          <line className="solution"
+                key="_solution"
+                x1={this.state.proposedSolution.start.x}
+                x2={this.state.proposedSolution.end.x}
+                y1={this.state.proposedSolution.start.y}
+                y2={this.state.proposedSolution.end.y} />}
+      </svg>
+    );
+  }
 }
 
-const mapStateToProps = ({ grid, words }) => (
-  {
-    grid,
-    hintedWordPositions: Object.values(words).filter(word => word.hinted)
-  }
-);
+const mapStateToProps = ({ grid, words }) => ({ grid, words: Object.values(words) });
 
-export default connect(mapStateToProps, {})(Grid);
+function mapDispatchToProps(dispatch) {
+  return {
+    attemptSolution: (start, end) => dispatch(attemptSolution(start, end))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Grid);
--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import _ from 'lodash';
 
 const TEST_STATE = {
   grid: [['H', 'I', 'X', 'Y', 'Z'],
@@ -8,22 +9,27 @@ const TEST_STATE = {
          ['Z', 'W', 'U', 'P', 'V']],
   words: {
     HI: {
+      word: 'HI',
       start: {x: 0, y: 0},
       end: {x: 1, y: 0}
     },
     HOW: {
+      word: 'HOW',
       start: {x: 0, y: 0},
       end: {x: 0, y: 2}
     },
     RUN: {
+      word: 'RUN',
       start: {x: 2, y: 1},
       end: {x: 4, y: 3}
     },
     RUM: {
+      word: 'RUM',
       start: {x: 2, y: 1},
       end: {x: 2, y: 3}
     },
     TRIP: {
+      word: 'TRIP',
       start: {x: 1, y: 1},
       end: {x: 4, y: 1}
     }
@@ -40,6 +46,17 @@ function words(state = TEST_STATE.words, action) {
       return { ...state,
         [action.word]: { ...state[action.word], hinted: action.hinted }
       };
+    case 'ATTEMPT_SOLUTION':
+      const word = Object.values(state).find(word =>
+        (_.isEqual(word.start, action.start) && _.isEqual(word.end, action.end)) ||
+        (_.isEqual(word.start, action.end) && _.isEqual(word.end, action.start)));
+      if (word) {
+        return { ...state,
+          [word.word]: { ...word, solved: true }
+        };
+      } else {
+        return state;
+      }
     default:
       return state;
   }
