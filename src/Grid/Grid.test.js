a352d4d16e2d375de0205b942c9ab2f85a6f979f
Jacob Williams <jacobaw@gmail.com>
2018-01-25 18:19:40 -0600

    fix tests and add test for solved word lines

--- src/Grid/Grid.test.js
+++ src/Grid/Grid.test.js
@@ -9,7 +9,7 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('<Grid />', () => {
   it('renders a <text /> for each letter on the grid', () => {
     const testGrid = [['A', 'B'], ['C', 'D']];
-    const wrapper = shallow(<Grid grid={testGrid} hintedWordPositions={[]} />);
+    const wrapper = shallow(<Grid grid={testGrid} words={[]} />);
     const letters = wrapper.find('text');
     expect(letters).toHaveLength(4);
     expect(letters.at(0).text()).toEqual('A');
@@ -19,24 +19,27 @@ describe('<Grid />', () => {
   });
 
   it('renders a <line /> for each hinted word', () => {
-    const testHints = [
+    const words = [
       {
         word: 'HI',
         start: {x: 1, y: 1},
-        end: {x: 3, y: 1}
+        end: {x: 3, y: 1},
+        hinted: true
       },
       {
         word: 'YO',
         start: {x: 1, y: 1},
-        end: {x: 1, y: 3}
+        end: {x: 1, y: 3},
+        hinted: true
       },
       {
         word: 'HEY',
         start: {x: 1, y: 2},
-        end: {x: 3, y: 4}
+        end: {x: 3, y: 4},
+        hinted: true
       }
     ];
-    const wrapper = shallow(<Grid grid={[['A']]} hintedWordPositions={testHints} />);
+    const wrapper = shallow(<Grid grid={[['A']]} words={words} />);
     const lines = wrapper.find('line .hint');
     expect(lines).toHaveLength(3);
     const horizontal = lines.get(0);
@@ -54,5 +57,61 @@ describe('<Grid />', () => {
     expect(diagonal.props.x2).toEqual(105);
     expect(diagonal.props.y1).toEqual(82);
     expect(diagonal.props.y2).toEqual(142);
+  });
+
+  it('renders a <line /> for each solved word', () => {
+    const words = [
+      {
+        word: 'HI',
+        start: {x: 1, y: 1},
+        end: {x: 3, y: 1},
+        solved: true
+      },
+      {
+        word: 'YO',
+        start: {x: 1, y: 1},
+        end: {x: 1, y: 3},
+        solved: true
+      },
+      {
+        word: 'HEY',
+        start: {x: 1, y: 2},
+        end: {x: 3, y: 4},
+        solved: true
+      }
+    ];
+    const wrapper = shallow(<Grid grid={[['A']]} words={words} />);
+    const lines = wrapper.find('line .solution');
+    expect(lines).toHaveLength(3);
+    const horizontal = lines.get(0);
+    expect(horizontal.props.x1).toEqual(45);
+    expect(horizontal.props.x2).toEqual(105);
+    expect(horizontal.props.y1).toEqual(52);
+    expect(horizontal.props.y2).toEqual(52);
+    const vertical = lines.get(1);
+    expect(vertical.props.x1).toEqual(45);
+    expect(vertical.props.x2).toEqual(45);
+    expect(vertical.props.y1).toEqual(52);
+    expect(vertical.props.y2).toEqual(112);
+    const diagonal = lines.get(2);
+    expect(diagonal.props.x1).toEqual(45);
+    expect(diagonal.props.x2).toEqual(105);
+    expect(diagonal.props.y1).toEqual(82);
+    expect(diagonal.props.y2).toEqual(142);
+  });
+
+  it('gives hints precedence over solutions', () => {
+    const words = [
+      {
+        word: 'HI',
+        start: {x: 1, y: 1},
+        end: {x: 3, y: 1},
+        solved: true,
+        hinted: true
+      }
+    ];
+    const wrapper = shallow(<Grid grid={[['A']]} words={words} />);
+    expect(wrapper.find('line .solution')).toHaveLength(0);
+    expect(wrapper.find('line .hint')).toHaveLength(1);
   });
 });
